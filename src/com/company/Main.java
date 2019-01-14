e6f8570020a675d6ea311682844918f2a54161ec
fvictoria <felipevictoria630@yahoo.com>
2019-01-14 11:20:28 -0500

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        Scanner kbReader = new Scanner(System.in);
+        System.out.println("So, you need help with Towers of Hanoi??");
+        String response = kbReader.nextLine();
+        System.out.println("Great!");
+        System.out.println("How many blocks do you have?");
+        int blocks = kbReader.nextInt();
+        
+        if(blocks == 3)
+        {
+
+        }
     }
 }
